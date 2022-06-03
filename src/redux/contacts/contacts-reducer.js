6324f9327a2b4397df215c15d6e3813f7ea5e296
Anton K <89594975+Pusttak@users.noreply.github.com>
2022-06-03 20:07:49 +0400

    finish

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -1,12 +1,7 @@
 import { createReducer, combineReducers } from '@reduxjs/toolkit';
 import * as actions from './contacts-actions';
 
-const itemsInitialState = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+const itemsInitialState = [];
 
 const itemsReducer = createReducer(itemsInitialState, {
   [actions.addContact]: (state, { payload }) => [...state, payload],
