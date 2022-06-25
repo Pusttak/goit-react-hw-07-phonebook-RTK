e73ceb274e2d2edc7dd46078674ee11c0592a3e8
Anton K <89594975+Pusttak@users.noreply.github.com>
2022-06-25 20:32:16 +0200

    finish

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,25 +6,13 @@ import { Container, Title, Subtitle } from './App.styled.jsx';
 
 const App = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'start',
-        alignItems: 'center',
-        fontSize: 40,
-        marginTop: '10px',
-        color: '#010101',
-      }}
-    >
-      <Container>
-        <Title>Phonebook</Title>
-        <ContactForm />
-        <Subtitle>Contacts</Subtitle>
-        <Filter />
-        <ContactList />
-      </Container>
-    </div>
+    <Container>
+      <Title>Phonebook</Title>
+      <ContactForm />
+      <Subtitle>Contacts</Subtitle>
+      <Filter />
+      <ContactList />
+    </Container>
   );
 };
 
--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
+  margin: 0 auto;
+  margin-top: 10px;
   padding: 10px;
   width: 370px;
 `;
